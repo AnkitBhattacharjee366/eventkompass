fbb19ee4bc81041fb881fb190363d717320883f6
AnkitBhattacharjee366 <bhattacharjeeankit366@gmail.com>
2026-01-18 16:14:48 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/eventkompass/',   // ✅ Required for GitHub Pages
+    base: '/eventkompass/',  // ✅ Required for GitHub Pages
 
     server: {
       port: 3000,
